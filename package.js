c8f7600f9cee2afd8f218efdb24fbd0b067238cf
Casey Henson <caseyahenson@gmail.com>
2019-07-24 09:32:43 -0700

    JS lib cleanup

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'joypixels:emoji-toolkit',
   summary: 'Meteor Package of the https://www.joypixels.com/ set.',
-  version: '5.0.1',
+  version: '5.0.2',
   git: 'https://github.com/joypixels/emoji-toolkit.git'
 });
 
