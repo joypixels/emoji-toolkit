3b6bc00d92b543657f057e2a48489fcbd9160c98
Casey Henson <caseyahenson@gmail.com>
2020-07-15 09:40:55 -0700

    data updates

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'joypixels:emoji-toolkit',
   summary: 'Meteor Package of the https://www.joypixels.com/ set.',
-  version: '6.0.0',
+  version: '6.0.1',
   git: 'https://github.com/joypixels/emoji-toolkit.git'
 });
 
